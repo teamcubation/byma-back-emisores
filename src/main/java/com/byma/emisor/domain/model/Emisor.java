450bfdf793a6a4908c5f194486772b0445891fb3
Heber Tescione <hebertescione@hotmail.com>
2024-11-11 16:27:47 -0300

    agrego un mapper para la persistencia de emisores

--- src/main/java/com/byma/emisor/domain/model/Emisor.java
+++ src/main/java/com/byma/emisor/domain/model/Emisor.java
@@ -1,4 +1,26 @@
 package com.byma.emisor.domain.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class Emisor {
+    private long id;
+
+    private String denominacion;
+
+    private String mail;
+
+    private LocalDate fechaAlta;
+
+    private String cuentaEmisor;
+
+    private long idOrganizacion;
+
+    private long idEntidadLegal;
+
+
 }
