bac4b3e635e2874d2e8ab971f3fd9388e9a8d345
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-12-24 13:47:08 -0300

    Modifico application.properties para obtener datos de conexión de db desde un .env

--- src/main/java/com/byma/emisor/EmisorApplication.java
+++ src/main/java/com/byma/emisor/EmisorApplication.java
@@ -2,11 +2,26 @@ package com.byma.emisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EmisorApplication {
 
 	public static void main(String[] args) {
+		try (Stream<String> stream = Files.lines(Paths.get(".env"))) {
+			stream.forEach(line -> {
+				String[] keyValue = line.split("=", 2);
+				if (keyValue.length == 2) {
+					System.setProperty(keyValue[0], keyValue[1]);
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(EmisorApplication.class, args);
 	}
 
